b38bca314619872444034c201aa0ec8348057a0a
Fredrick Njoroge <fredrick.kariuki@mpesafoundationacademy.ac.ke>
2025-06-29 12:48:52 +0300

    Your commit message

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+ import './index.css'
 
 // Check if we're on a static HTML page
 const isStaticPage = window.location.pathname.includes('.html') || 
